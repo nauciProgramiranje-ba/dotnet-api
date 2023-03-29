27a485ff2fde26adcc86456ea6b051ab468cd814
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-03-29 10:59:18 +0200

    Switched to minimal API

--- db/DbScript.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE nauciProgramiranje;
-
-USE nauciProgramiranje;
-
-CREATE TABLE Lekcija(
-    Lekcija_ID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    VideoURL NVARCHAR(255) NOT NULL,
-    Naslov NVARCHAR(255) NOT NULL,
-)
