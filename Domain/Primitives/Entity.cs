cfd19a54c3a4bdb62f84cd97374b9be81bcb5237
edin.sahbaz <edin.sahbaz@hotmail.com>
2023-05-26 13:02:29 +0200

    Arhitecture tests

---
+++ Domain/Primitives/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Primitives;
+
+public abstract class Entity
+{
+    protected Entity(Guid id) => Id = id;
+
+    protected Entity()
+    {
+    }
+
+    public Guid Id { get; protected set; }
+}
---
+++ WebAPI/AssemblyReference.cs
@@ -0,0 +1,5 @@
+﻿namespace WebAPI;
+
+public class AssemblyReference
+{
+}
