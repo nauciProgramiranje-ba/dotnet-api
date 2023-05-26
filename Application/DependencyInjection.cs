407d1c4ed3b2945ec268f687c979ea2d43086847
edin.sahbaz <edin.sahbaz@hotmail.com>
2023-05-26 11:36:04 +0200

    Configured DI

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration => 
+            configuration.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssemblies((IEnumerable<Assembly>)assembly);
+        
+        return services;
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
