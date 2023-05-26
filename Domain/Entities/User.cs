bf0ccc519e91262e9e1263b5d27734da718ef48a
edin.sahbaz <edin.sahbaz@hotmail.com>
2023-05-26 14:52:44 +0200

    Entities

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public sealed class User : Entity
+{
+    private User(Guid id, string name, string email) 
+        : base(id)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+}
--- Domain/Primitives/Entity.cs
+++ Domain/Primitives/Entity.cs
@@ -1,12 +1,58 @@
 ﻿namespace Domain.Primitives;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
     protected Entity(Guid id) => Id = id;
 
-    protected Entity()
+    public Guid Id { get; private init; }
+
+    public static bool operator ==(Entity? first, Entity? second)
     {
+        return first is not null && second is not null && first.Equals(second);
     }
 
-    public Guid Id { get; protected set; }
+    public static bool operator !=(Entity? first, Entity? second)
+    {
+        return !(first == second);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if(GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        if(obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        return other.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
